1d15993c9b32623dc89789082141ed34045ce5bc
GitHub Action <action@github.com>
2021-06-06 19:14:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/segersb/foobarintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.segersb.foobarintellijplugin.services
+
+import com.github.segersb.foobarintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/segersb/foobarintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/segersb/foobarintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.segersb.foobarintellijplugin.services
 
+import com.github.segersb.foobarintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
